aea5af0407931817422633408eec7537b769deca
Touba Derakhshandeh <tderakhshande@gmail.com>
2025-10-14 11:22:05 +0330

    add matching model

--- modules/matching/app.py
+++ modules/matching/app.py
@@ -1,1 +1,25 @@
-# app.py 
+# matcher.py
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+class FaceMatcher:
+    def __init__(self, known_embeddings, known_labels, threshold=0.5):
+        """
+        known_embeddings: np.array (N, D)
+        known_labels: known people
+        threshold: similarity threshold
+        """
+        self.known_embeddings = known_embeddings
+        self.known_labels = known_labels
+        self.threshold = threshold
+
+    def match(self, embeddings):
+        results = []
+        for emb in embeddings:
+            sims = cosine_similarity([emb], self.known_embeddings)[0]
+            max_idx = np.argmax(sims)
+            if sims[max_idx] > self.threshold:
+                results.append(self.known_labels[max_idx])
+            else:
+                results.append("Unknown")
+        return results
