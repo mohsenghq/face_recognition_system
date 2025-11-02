5668db6573af5c81f371b0ca754d9bad08e0727b
Touba Derakhshandeh <tderakhshande@gmail.com>
2025-11-02 17:32:52 +0330

    update tracker model

--- modules/tracker/app.py
+++ modules/tracker/app.py
@@ -1,34 +1,54 @@
-# tracker.py
-import cv2
-from yolox.tracker.byte_tracker import BYTETracker, STrack
 import numpy as np
+from cjm_byte_track.core import BYTETracker
 
 class FaceTracker:
     def __init__(self, fps=30, track_buffer=30):
-        self.tracker = BYTETracker(fps=fps, track_buffer=track_buffer)
+        self.tracker = BYTETracker(
+            track_thresh=0.5,
+            track_buffer=track_buffer,
+            match_thresh=0.8,
+            frame_rate=fps
+        )
 
-    def update(self, detections, img_shape):
+    def update(self, face_detections, img_shape):
         """
-        detections: bbox و score from RetinaFace
-        img_shape: ابعاد تصویر (height, width)
+        face_detections: bbox و score 
+            example:
+            [
+                {"bbox": [x1, y1, x2, y2], "score": 0.98, "id": "person_A"},
+                {"bbox": [x1, y1, x2, y2], "score": 0.95, "id": "person_B"},
+            ]
+        img_shape: (height, width)
         """
         height, width = img_shape[:2]
 
-        # change format
+        if not face_detections:
+            return []
+
+        # input of ByteTracker
         dets = []
-        for det in detections:
-            x1, y1, x2, y2 = det['bbox']
-            score = det.get('score', 1.0)
-            dets.append([x1, y1, x2, y2, score])
+        ids = []
+        for det in face_detections:
+            x1, y1, x2, y2 = det["bbox"]
+            score = det.get("score", 1.0)
+            dets.append([x1, y1, x2, y2, float(score)])
+            ids.append(det.get("id"))  # id matching
+
         dets = np.array(dets, dtype=np.float32)
 
-        # run tracker
-        tracks = self.tracker.update(dets, [height, width], (height, width))
+        # tracking
+        tracks = self.tracker.update(dets)
 
-        # outputs
+        # matching
         track_results = []
-        for track in tracks:
-            bbox = track.tlbr  # [x1, y1, x2, y2]
+        for i, track in enumerate(tracks):
+            bbox = track.tlbr
             track_id = track.track_id
-            track_results.append({'track_id': track_id, 'bbox': bbox})
+            person_id = ids[i] if i < len(ids) else None
+            track_results.append({
+                "track_id": track_id,
+                "bbox": bbox,
+                "person_id": person_id
+            })
+
         return track_results
